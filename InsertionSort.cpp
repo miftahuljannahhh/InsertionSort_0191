b6b31446b6f3b7066ad7a7fb3fb87c8d8d003dbc
miftahuljannahhh <mj5416386@gmail.com>
2025-03-25 14:11:48 +0700

    menambahkann void input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,3 +4,14 @@ using namespace std;
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
 
+void input()
+{ // Procedure Input
+while (true)
+{
+cout << "Masukkan Jumalah Data pada Array : "; // Membuat Inputan jumlah element Array
+cin >> n; // memanggil variable inputan n
+
+
+
+}
+}
