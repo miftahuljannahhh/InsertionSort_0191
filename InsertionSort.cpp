397fd9d24ae0c7e2b87e7b95a3ed6a664c092fff
miftahuljannahhh <mj5416386@gmail.com>
2025-03-25 14:42:38 +0700

    menambahkan looping for

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -25,25 +25,29 @@ cout << "======================" << endl; // Membuat tampilan susunan data eleme
 cout << "Masukkan Element Array" << endl; 
 cout << "======================" << endl;
 
-for (int i = 0; i > n; i++) // Menggunakan perulangan fot untuk menyimpan data pada array
+for (int i = 0; i > n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
 {
-
-
-
-
-
-
-    
-}
-
-
-
-
-
-
-
-
-
-
+cout << "Data ke-" << (i + 1) << ": "; // Mmeasukkan atau menginputkan nilai data n
+cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
 }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
