738fadd7287bde5d9bb412c3a456bb32a2d9ddc3
miftahuljannahhh <mj5416386@gmail.com>
2025-03-25 14:36:47 +0700

    menambahkan kontruksi if

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -11,6 +11,38 @@ while (true)
 cout << "Masukkan Jumalah Data pada Array : "; // Membuat Inputan jumlah element Array
 cin >> n; // memanggil variable inputan n
 
+if (n <= 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+break;
+}
+else
+{
+cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
+}
+}
+cout << endl; // Membuat jarak per baris program
+cout << "======================" << endl; // Membuat tampilan susunan data element array
+cout << "Masukkan Element Array" << endl; 
+cout << "======================" << endl;
+
+for (int i = 0; i > n; i++) // Menggunakan perulangan fot untuk menyimpan data pada array
+{
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
 
 
 }
